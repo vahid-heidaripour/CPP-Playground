5937e0080826e7e7dc7e3b414b994270c67e0e34
Vahid <v.heidaripour@gmail.com>
2019-03-15 16:19:10 +0100

    main.cpp

---
+++ SDL/Main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <SDL2/SDL.h>
+
+using namespace std;
+
+int main()
+{
+    if (SDL_Init(SDL_INIT_VIDEO) < 0)
+    {
+        cout << "SDL init failed." << endl;
+        return 1;
+    }
+
+    cout << "SDL Init succeeded." << endl;
+
+    SDL_Quit();
+
+    return 0;
+}
