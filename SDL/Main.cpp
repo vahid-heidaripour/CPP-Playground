d7e1542fc4cb9569f39bd06c264029ff636be71a
Vahid <v.heidaripour@gmail.com>
2019-03-15 16:42:46 +0100

    first window

--- SDL/Main.cpp
+++ SDL/Main.cpp
@@ -5,6 +5,9 @@ using namespace std;
 
 int main()
 {
+    const int SCREEN_WIDTH = 800;
+    const int SCREEN_HEIGHT = 600;
+
     if (SDL_Init(SDL_INIT_VIDEO) < 0)
     {
         cout << "SDL init failed." << endl;
@@ -13,6 +16,29 @@ int main()
 
     cout << "SDL Init succeeded." << endl;
 
+    SDL_Window *window = SDL_CreateWindow("test",
+            SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH,
+            SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+
+    if (window == NULL)
+    {
+        cout << "Could not create window " << SDL_GetError() << endl;
+        SDL_Quit();
+        return 2;
+    }
+
+    bool quit = false;
+    SDL_Event event;
+
+    while (!quit) {
+        while (SDL_PollEvent(&event))
+        {
+            if (event.type == SDL_QUIT)
+                quit = true;
+        }
+    }
+
+    SDL_DestroyWindow(window);
     SDL_Quit();
 
     return 0;
