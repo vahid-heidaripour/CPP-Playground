75414d150f4fa81430c083fb9172df8804624139
Vahid <v.heidaripour@gmail.com>
2019-03-15 18:39:40 +0100

    animating colors

--- SDL/Main.cpp
+++ SDL/Main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <SDL2/SDL.h>
+#include <math.h>
 #include "Screen.h"
 
 using namespace std;
@@ -16,15 +17,19 @@ int main()
 
     while (true)
     {
+        int elapsed = SDL_GetTicks();
+
+        unsigned char red = (unsigned char)((1 + sin(elapsed * 0.0001)) * 128);
+        unsigned char green = (unsigned char)((1 + sin(elapsed * 0.0002)) * 128);
+        unsigned char blue = (unsigned char)((1 + sin(elapsed * 0.0003)) * 128);
+
         for (int y = 0; y < Screen::SCREEN_HEIGHT; y++)
         {
             for (int x = 0; x < Screen::SCREEN_WIDTH; x++)
             {
-                screen.setPixel(x, y, 128, 0, 255);
+                screen.setPixel(x, y, red, green, blue);
             }
         }
-
-        screen.setPixel(400, 300, 255, 255, 255);
 
         screen.update();
 
