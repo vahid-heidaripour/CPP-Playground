f57fcd9027185a858f436d716bb161eeae3d14f1
Vahid <v.heidaripour@gmail.com>
2019-03-19 20:27:53 +0100

    main thread

--- Concurrent/protecting_shared_mutex.cpp
+++ Concurrent/protecting_shared_mutex.cpp
@@ -23,7 +23,11 @@ void size()
 
 int main(int argc, char* argv[])
 {
+    std::thread thread_1(add_to_list, 4);
+    std::thread thread_2(add_to_list, 11);
 
+    thread_1.join();
+    thread_2.join();
 
     return 0;
 }
