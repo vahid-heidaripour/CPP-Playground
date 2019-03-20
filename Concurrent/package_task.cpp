40f6165d0610570ac89137950b19eb18324bd9e5
Vahid <v.heidaripour@gmail.com>
2019-03-20 17:56:11 +0100

    init

---
+++ Concurrent/package_task.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <future>
+#include <numeric>
+#include <thread>
+#include <functional>
+
+int add(int x, int y)
+{
+
+}
+
+void task_thread()
+{
+
+}
+
+void task_normal()
+{
+
+}
+
+int main(int argc, char* argv[])
+{
+    return 0;
+}
