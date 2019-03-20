d658233fd6e21c41d28c5f78c56dcb3f3b19af6f
Vahid <v.heidaripour@gmail.com>
2019-03-20 17:44:16 +0100

    init

---
+++ Concurrent/parallel_accumulate_async.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+#include <future>
+#include <numeric>
+#include <iterator>
+
+const int MIN_ELEMENT_COUNT = 1000;
+
+template <typename iterator>
+int parallel_accumulate(iterator begin, iterator end)
+{
+
+}
+
+int main(int argc, char* argv[])
+{
+    return 0;
+}
