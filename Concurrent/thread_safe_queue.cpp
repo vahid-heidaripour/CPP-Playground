a85ad95d52e326fbfea0fa0fdeaa1dc454f6d2ac
Vahid <v.heidaripour@gmail.com>
2019-03-20 14:14:46 +0100

    functions

--- Concurrent/thread_safe_queue.cpp
+++ Concurrent/thread_safe_queue.cpp
@@ -28,17 +28,34 @@ public:
 
     std::shared_ptr<T> pop()
     {
-
+        std::lock_guard<std::mutex> lg(m);
+        if (queue.empty())
+        {
+            return std::shared_ptr<T>();
+        }
+        else
+        {
+            std::shared_ptr<T> ref(queue.front());
+            queue.pop();
+            return  ref;
+        }
     }
 
     bool empty()
     {
-
+        std::lock_guard<std::mutex> lg(m);
+        return queue.empty();
     }
 
     std::shared_ptr<T> wait_pop()
     {
-
+        std::unique_lock<std::mutex> lg(m);
+        cv.wait(lg, [this] {
+            return !queue.empty();
+        });
+        std::shared_ptr<T> ref = queue.front();
+        queue.pop();
+        return ref;
     }
 
     size_t size()
