34c91af62c151a309d2806da30ea4d9d15b87303
Vahid <v.heidaripour@gmail.com>
2019-03-20 11:55:08 +0100

    bus example

---
+++ Concurrent/bus_example.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <mutex>
+#include <thread>
+#include <string>
+#include <chrono>
+
+bool have_i_arrived = false;
+int distance_my_destination = 10;
+int distance_covered = 0;
+
+bool keep_driving()
+{
+
+}
+
+void keep_awake_all_night()
+{
+
+}
+
+void set_the_alarm_and_take_nap()
+{
+
+}
+
+int main(int argc, char* argv[])
+{
+    return 0;
+}
