586e472c4a22a1a8c79949d06d310109d08d8041
Vahid <v.heidaripour@gmail.com>
2019-03-21 21:52:59 +0100

    textures

--- OpenGL/OpenGL-Test/OpenGL-Test/main.cpp
+++ OpenGL/OpenGL-Test/OpenGL-Test/main.cpp
@@ -2,90 +2,144 @@
 
 #define GLEW_STATIC
 #include <GL/glew.h>
+
 #include <GLFW/glfw3.h>
+
+#include "SOIL2/SOIL2.h"
 
 #include "shader.h"
 
-const GLint WIDTH = 800, HEIGHT = 600;
+const GLuint WIDTH = 800, HEIGHT = 600;
 
 int main(int argc, char* argv[])
 {
-    glfwInit();
+    glfwInit( );
     
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+    glfwWindowHint( GLFW_CONTEXT_VERSION_MAJOR, 3 );
+    glfwWindowHint( GLFW_CONTEXT_VERSION_MINOR, 3 );
+    glfwWindowHint( GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE );
+    glfwWindowHint( GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE );
     
-    GLFWwindow *window = glfwCreateWindow(WIDTH, HEIGHT, "Learn OpenGL", nullptr, nullptr);
+    glfwWindowHint( GLFW_RESIZABLE, GL_FALSE );
+    
+    GLFWwindow *window = glfwCreateWindow( WIDTH, HEIGHT, "LearnOpenGL", nullptr, nullptr );
     
     int screenWidth, screenHeight;
-    glfwGetFramebufferSize(window, &screenWidth, &screenHeight);
+    glfwGetFramebufferSize( window, &screenWidth, &screenHeight );
     
-    if (nullptr == window)
+    if ( nullptr == window )
     {
         std::cout << "Failed to create GLFW window" << std::endl;
-        glfwTerminate();
+        glfwTerminate( );
         
-        return -1;
+        return EXIT_FAILURE;
     }
     
-    glfwMakeContextCurrent(window);
-    
+    glfwMakeContextCurrent( window );
+   
     glewExperimental = GL_TRUE;
     
-    if (GLEW_OK != glewInit())
+    if ( GLEW_OK != glewInit( ) )
     {
         std::cout << "Failed to initialize GLEW" << std::endl;
-        return -1;
+        return EXIT_FAILURE;
     }
     
-    glViewport(0, 0, screenWidth, screenHeight);
     
-    Shader ourShader("./resources/shaders/core.vs", "./resources/shaders/core.frag");
+    glViewport( 0, 0, screenWidth, screenHeight );
+    
+    
+    glEnable( GL_BLEND );
+    glBlendFunc( GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA );
+    
+    
+    Shader ourShader( "resources/shaders/core.vs", "resources/shaders/core.frag" );
     
     GLfloat vertices[] =
     {
-        -0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f,
-        0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f,
-        0.0f, 0.5f, 0.0f, 0.0f, 0.0f, 1.0f
+        0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f,
+        0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f,
+        -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f,
+        -0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f
     };
-    
-    GLuint VBO, VAO;
-    glGenVertexArrays(1, &VAO);
-    glGenBuffers(1, &VBO);
-    
-    glBindVertexArray(VAO);
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-    
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*) 0);
-    glEnableVertexAttribArray(0);
-    
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
-    glEnableVertexAttribArray(1);
-    
-    glBindVertexArray(0);
-    
-    while (!glfwWindowShouldClose(window))
+    GLuint indices[] =
     {
-        glfwPollEvents();
-        glClearColor(0.3f, 0.2f, 0.1f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
+        0, 1, 3,
+        1, 2, 3
+    };
+    GLuint VBO, VAO, EBO;
+    glGenVertexArrays( 1, &VAO );
+    glGenBuffers( 1, &VBO );
+    glGenBuffers( 1, &EBO );
+    
+    glBindVertexArray( VAO );
+    
+    glBindBuffer( GL_ARRAY_BUFFER, VBO );
+    glBufferData( GL_ARRAY_BUFFER, sizeof( vertices ), vertices, GL_STATIC_DRAW );
+    
+    glBindBuffer( GL_ELEMENT_ARRAY_BUFFER, EBO );
+    glBufferData( GL_ELEMENT_ARRAY_BUFFER, sizeof( indices ), indices, GL_STATIC_DRAW );
+    
+    // Position attribute
+    glVertexAttribPointer( 0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof( GLfloat ), ( GLvoid * ) 0 );
+    glEnableVertexAttribArray(0);
+    // Color attribute
+    glVertexAttribPointer( 1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof( GLfloat ), ( GLvoid * )( 3 * sizeof( GLfloat ) ) );
+    glEnableVertexAttribArray(1);
+    // Texture Coordinate attribute
+    glVertexAttribPointer( 2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof( GLfloat ), ( GLvoid * )( 6 * sizeof( GLfloat ) ) );
+    glEnableVertexAttribArray( 2 );
+    
+    glBindVertexArray( 0 );
+    
+    
+    GLuint texture;
+    
+    int width, height;
+    
+    glGenTextures( 1, &texture );
+    glBindTexture( GL_TEXTURE_2D, texture );
+ 
+    glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT );
+    glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT );
+    
+    glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR );
+    glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR );
+    
+    unsigned char *image = SOIL_load_image( "resources/images/image_1.jpg", &width, &height, 0, SOIL_LOAD_RGBA );
+    glTexImage2D( GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, image );
+    glGenerateMipmap( GL_TEXTURE_2D );
+    SOIL_free_image_data( image );
+    glBindTexture( GL_TEXTURE_2D, 0 );
+    
+
+    while ( !glfwWindowShouldClose( window ) )
+    {
+        glfwPollEvents( );
         
-        ourShader.Use();
-        glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
-        glBindVertexArray(0);
+        glClearColor( 0.2f, 0.3f, 0.3f, 1.0f );
+        glClear( GL_COLOR_BUFFER_BIT );
         
-        glfwSwapBuffers(window);
+        ourShader.Use( );
+        glActiveTexture( GL_TEXTURE0 );
+        glBindTexture( GL_TEXTURE_2D, texture );
+        glUniform1i( glGetUniformLocation( ourShader.Program, "ourTexture" ), 0 );
+        
+        glBindVertexArray( VAO );
+        glDrawElements( GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0 );
+        glBindVertexArray( 0 );
+        
+        glfwSwapBuffers( window );
     }
     
-    glDeleteVertexArrays(1, &VAO);
-    glDeleteBuffers(1, &VBO);
+    glDeleteVertexArrays( 1, &VAO );
+    glDeleteBuffers( 1, &VBO );
+    glDeleteBuffers( 1, &EBO );
     
-    glfwTerminate();
+    glfwTerminate( );
     
-    return 0;
+    return EXIT_SUCCESS;
 }
+
+
+
--- OpenGL/OpenGL-Test/OpenGL-Test/resources/shaders/core.frag
+++ OpenGL/OpenGL-Test/OpenGL-Test/resources/shaders/core.frag
@@ -1,9 +1,13 @@
 #version 330 core
 
 in vec3 ourColor;
+in vec2 TexCoord;
 
 out vec4 color;
+
+uniform sampler2D ourTexture1;
+
 void main()
 {
-    color = vec4(ourColor, 1.0f);
+    color = texture(ourTexture1, TexCoord);
 }
