90082320eabd8b4f75d3d8ee0cf6d6792dd69a20
Vahid <v.heidaripour@gmail.com>
2019-03-29 12:45:07 +0100

    \c warning

--- main.c
+++ main.c
@@ -3,6 +3,8 @@
 int main() /* without int generates warning : type specifier missing, defaults to 'int' */
 {
     printf("Hello, World\n");
+    /* printf("Hello, World\c\n");
+     *  warning: unknown escape sequence '\c' */
 }
 
 /* cc main.c */
