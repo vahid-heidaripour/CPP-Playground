ac6cb4d993d1e17b20ad49475a627a3fd2734ac7
Vahid <v.heidaripour@gmail.com>
2019-04-02 17:30:17 +0200

    virtual inherentece

--- diamond_problem.cpp
+++ diamond_problem.cpp
@@ -14,17 +14,28 @@ public:
     int _a;
 };
 
-class B : public A
+// A object memory layout
+// _a
+
+class B : virtual public A
 {
 public:
     int _b;
 };
 
-class C : public A
+// B object memory layout
+// _b, _a
+// size of B is sizeof(_b) + sizeof(_a)
+
+class C : virtual public A
 {
 public:
     int _c;
 };
+
+// C object memory layout
+// _c, _a
+// size of C is sizeof(_c) + sizeof(_a)
 
 class D : public B, public C
 {
@@ -32,7 +43,15 @@ public:
     int _d;
 };
 
+// D object memory layout
+// _a, _b, _c, _a, _d
+
 int main()
 {
+    D d; // no problem
+    d._b = 10; // OK
+    d._c = 10; // OK
+    d._d = 10; // OK
+    d._a = 10; // here is the ambiguity problem, to solve it we need to inherent from A virtually
     return 0;
 }
