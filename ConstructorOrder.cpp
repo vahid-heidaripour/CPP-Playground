0b7bf5ea43f6a2646c14ec52c4c08dd35352f32d
unknown <v.heidaripour@gmail.com>
2020-01-03 17:27:23 +0330

    destructors order

--- ConstructorOrder.cpp
+++ ConstructorOrder.cpp
@@ -6,18 +6,21 @@ class Something
 {
 public:
     Something() { cout << 2 << endl; }
+	virtual ~Something() { cout << 2 << endl; }
 };
 
 class Base
 {
 public:
     Base() { cout << 1 << endl; }
+	virtual ~Base() { cout << 1 << endl; }
 };
 
 class Derived : public Base
 {
 public:
     Derived() { cout << 3 << endl; }
+	virtual ~Derived() { cout << 3 << endl; }
 
 private:
     Something mDataMember;
@@ -33,5 +36,6 @@ int main()
 /* When the myDerived object is created,
  * the constructor for Base is called first, outputting the 1.
  * Next, mDataMember is initialized, calling the Something constructor, which outputs the 2.
- * Finally, the Derived constructor is called, which outputs 3
+ * Finally, the Derived constructor is called, which outputs 3.
+ * The output is 123321
  */
