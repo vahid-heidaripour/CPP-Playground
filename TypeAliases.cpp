b9b59c9782fa0c7e4163ec8fdfc46224b43cb6cf
unknown <v.heidaripour@gmail.com>
2020-01-05 11:42:36 +0330

    what is type aliases?

---
+++ TypeAliases.cpp
@@ -0,0 +1,22 @@
+#include <vector>
+#include <string>
+
+/*void processVector(const std::vector<std::string>& vec)
+{
+
+}*/
+
+using StringVector = std::vector<std::string>;
+
+void processVector(const StringVector& vec)
+{
+
+}
+
+int main()
+{
+    StringVector myVector;
+    processVector(myVector);
+
+    return 0;
+}
