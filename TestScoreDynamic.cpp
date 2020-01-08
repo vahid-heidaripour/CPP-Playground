f6f6147e49c7007c9a1581d509f42129e67a349b
unknown <v.heidaripour@gmail.com>
2020-01-08 15:38:35 +0330

    dynamic vector allocation

---
+++ TestScoreDynamic.cpp
@@ -0,0 +1,36 @@
+#include <cstddef>
+#include <vector>
+#include <iostream>
+#include <limits>
+
+using namespace std;
+
+int main()
+{
+    vector<double> doubleVector;
+
+    double max = -numeric_limits<double>::infinity();
+
+    for (size_t i = 0; true; i++) {
+        double temp;
+        cout << "Enter score " << i << " (-1 to stop): ";
+        cin >> temp;
+        if (temp == -1) {
+            break;
+        }
+
+        doubleVector.push_back(temp);
+        if (temp > max) {
+            max = temp;
+        }
+    }
+
+    max /= 100.0;
+    for (auto& element : doubleVector) {
+        element /= max;
+        cout << element << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
