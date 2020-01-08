fefca16d70f13c19550a3de8cd4258c70a774ad6
unknown <v.heidaripour@gmail.com>
2020-01-08 14:16:00 +0330

    toy program for vector and limits

---
+++ TestScores.cpp
@@ -0,0 +1,32 @@
+#include <cstddef>
+#include <vector>
+#include <iostream>
+#include <limits>
+
+using namespace std;
+
+int main()
+{
+    vector<double> doubleVector(10);
+
+    double max = -numeric_limits<double>::infinity();
+
+    for (size_t i = 0; i < doubleVector.size(); i++) {
+        cout << "Enter score " << i + 1 << ": ";
+        cin >> doubleVector[i];
+        if (doubleVector[i] > max) {
+            max = doubleVector[i];
+        }
+    }
+
+    max /= 100.0;
+
+    for (auto& element : doubleVector) {
+        element /= max;
+        cout << element << " ";
+    }
+
+    cout << endl;
+
+    return 0;
+}
